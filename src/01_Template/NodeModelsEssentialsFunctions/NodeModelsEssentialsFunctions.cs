893963f7a4516c2eb47cfd5a41adc818dfe7b6d5
Nono Martinez Alonso <mundowarezweb@gmail.com>
2017-08-22 10:13:11 +0200

    Simplify template; Add Surface sample .dyn

--- src/01_Template/NodeModelsEssentialsFunctions/NodeModelsEssentialsFunctions.cs
+++ src/01_Template/NodeModelsEssentialsFunctions/NodeModelsEssentialsFunctions.cs
@@ -10,20 +10,5 @@ namespace NodeModelsEssentials.Functions
         {
             return a * b;
         }
-
-        public static double Add(double a, double b)
-        {
-            return a + b;
-        }
-
-        public static double Subtract(double a, double b)
-        {
-            return a - b;
-        }
-
-        public static double Divide(double a, double b)
-        {
-            return a / b;
-        }
     }
 }
