bcd153a05f0ed18a99d9d7c16f2ec5104f8c4a71
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-12-07 10:50:08 -0500

    Add AstReusingFunctionCall and NotReusing; Helper to WriteLine to file.

--- src/Essentials/NodeModelsEssentials/EssentialsAstNotReusingFunctionCall.cs
+++ src/Essentials/NodeModelsEssentials/EssentialsAstNotReusingFunctionCall.cs
@@ -3,34 +3,54 @@ using System.Collections.Generic;
 using Dynamo.Graph.Nodes;
 using ProtoCore.AST.AssociativeAST;
 using NodeModelsEssentials.Functions;
+using Newtonsoft.Json;
 using System.Linq;
 
-namespace NodeModelsEssentials.Examples
+namespace NodeModelsEssentials
 {
     // <summary>
-    // Sample Node Model called MultiplyMulti.
-    // It returns the product of two numbers and a verbose string.
+    // This node displays a non-desirable way of using the AST.
+    // We pass function call nodes (say, multiplyFunctionNode)
+    // both to the sumFunctionNode and to one of the node outputs.
+    // This causes the AST to execute the multiply function twice.
+    // In this case, the overload is not too big, but for CPU-intensive
+    // tasks or for non-deterministic results (where the returning
+    // value of a function might be different two consecutive executions)
+    // the sum node and the output port will have different values.
     // </summary>
-    [NodeName("Essentials.AstReuseFunctionCall")]
+    [NodeName("Essentials.AstNotReusingFunctionCall")]
     [NodeDescription("Performs multiple operations with the inputs.")]
     [NodeCategory("NodeModelsEssentials")]
     [InPortNames("A", "B")]
     [InPortTypes("double", "double")]
     [InPortDescriptions("Number A", "Number B")]
-    [OutPortNames("AxB", "A+B", "A-B", "A/B")]
-    [OutPortTypes("double", "double")]
+    [OutPortNames("AxB", "A+B", "A-B", "A/B", "SUM")]
+    [OutPortTypes("double", "double", "double", "double", "double")]
     [OutPortDescriptions(
         "Product of A x B",
         "Addition of A and B",
         "Subtraction of A and B",
-        "Division of A by B")]
+        "Division of A by B",
+        "Sum of all outputs")]
     [IsDesignScriptCompatible]
-    public class MultiOperationReusingAst : NodeModel
+    public class AstNotReusingFunctionCall : NodeModel
     {
-        public MultiOperationReusingAst()
+
+        #region constructor
+
+        [JsonConstructor]
+        private AstNotReusingFunctionCall(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(inPorts, outPorts)
+        {
+        }
+
+        public AstNotReusingFunctionCall()
         {
             RegisterAllPorts();
         }
+
+        #endregion
+
+        #region ast
 
         public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAsNodes)
         {
@@ -41,7 +61,7 @@ namespace NodeModelsEssentials.Examples
                     AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(1), AstFactory.BuildIntNode(2)),
                     AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(2), AstFactory.BuildIntNode(3)),
                     AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(3), AstFactory.BuildIntNode(4)),
-                    AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(3), AstFactory.BuildIntNode(5))
+                    AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(4), AstFactory.BuildIntNode(5))
                 };
             }
 
@@ -65,22 +85,24 @@ namespace NodeModelsEssentials.Examples
                     new Func<double, double, double>(NodeModelsEssentialsFunctions.Divide),
                     new List<AssociativeNode> { inputAsNodes[0], inputAsNodes[1] });
 
-            var multiplyNodeIdentifier = AstFactory.BuildIdentifier(GUID.ToString() + "_multiply");
-            var multiplyNode = AstFactory.BuildAssignment(multiplyNodeIdentifier, multiplyFunctionNode);
-
-
-            //var sumFunctionNode =
-            //    AstFactory.BuildFunctionCall(
-            //        new Func<double, double, double, double, double>(NodeModelsEssentialsFunctions.Sum),
-            //        new List<AssociateNode> { multiplyNode, addFunctionNode, subtractFunctionNode, divideFunctionNode });
+            var sumFunctionNode =
+                AstFactory.BuildFunctionCall(
+                    new Func<double, double, double, double, double>(NodeModelsEssentialsFunctions.Sum),
+                    new List<AssociativeNode> {
+                        multiplyFunctionNode,
+                        addFunctionNode,
+                        subtractFunctionNode,
+                        divideFunctionNode });
 
             return new[] {
                 AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), multiplyFunctionNode),
                 AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(1), addFunctionNode),
                 AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(2), subtractFunctionNode),
                 AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(3), divideFunctionNode),
-               // AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(4), sumFunctionNode),
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(4), sumFunctionNode),
             };
         }
+
+        #endregion
     }
 }
